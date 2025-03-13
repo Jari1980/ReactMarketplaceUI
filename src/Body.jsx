8e33ce2efd0d10e8462bee5e444003a3e6c1b1aa
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-03-13 11:23:07 +0100

    card component added, body filled with data

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,12 +1,73 @@
 
 import './App.css'
+import CardComp from './CardComp'
+import CardGroup from 'react-bootstrap/CardGroup';
 
 function App() {
-  
+
+  const advertisementsData = [
+    {
+        id: 1,
+        title: 'Ad 1',
+        description: 'This is the first advertisement',
+        image: 'https://placehold.co/150',
+        contact: 'contact1@example.com'
+    },
+    {
+        id: 2,
+        title: 'Ad 2',
+        description: 'This is the second advertisement',
+        image: 'https://placehold.co/150',
+        contact: 'contact2@example.com'
+    },
+    {
+        id: 3,
+        title: 'Ad 3',
+        description: 'This is the third advertisement',
+        image: 'https://placehold.co/150',
+        contact: 'contact3@example.com'
+    },
+    {
+        id: 4,
+        title: 'Ad 4',
+        description: 'This is the fouth advertisement',
+        image: 'https://placehold.co/150',
+        contact: 'contact4@example.com'
+    },
+    {
+        id: 5,
+        title: 'Ad 5',
+        description: 'This is the fifth advertisement',
+        image: 'https://placehold.co/150',
+        contact: 'contact5@example.com'
+    },
+    {
+        id: 6,
+        title: 'Ad 6',
+        description: 'This is the sixth advertisement',
+        image: 'https://placehold.co/150',
+        contact: 'contact6@example.com'
+    }
+];
+
+const cardElements = advertisementsData.map((data) => {
+  const cardElement = (
+    <CardComp title={data.title} description={data.description} img={data.image} contact={data.contact}/>
+  )
+  return cardElement;
+})
+
 
   return (
     <>
-      <div>Body</div>
+      <div className='container'>
+        <h2>Advertisement list</h2>
+        <div className='container'>
+          <div className='row row-cols-1 row-cols-md-3 g-5'>
+          {cardElements}
+          </div>
+        </div>
+        </div>
     </>
   )
 }
---
+++ src/CardComp.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+
+
+const CardComp = (props) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.img} alt="..."/>
+      <Card.Body>
+        <Card.Title>{props.title} Title</Card.Title>
+        <Card.Text>
+          {props.description}
+          <br/>
+          <br/>
+          {props.contact}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CardComp;
