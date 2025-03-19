1f7629e4093079cf9d8c24eaec2d09a8752b10c4
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-03-19 14:07:05 +0100

    State and better style added to toggle background button

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,11 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { BrowserRouter, Link } from "react-router-dom";
 import { useCounterContext } from "./context";
-import { ToggleButton } from "react-bootstrap";
+import { ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+
 
 
 
@@ -17,24 +18,28 @@ const NavbarComp = () => {
   const {background, setBackground} = useCounterContext("");
   const {heading, setHeading} = useCounterContext("");
 
+  const [toggled, setToggle] = useState(false);
+
   function handleClick() {
     if(background === "linear-gradient(0deg,#f0e6e600, #d6d4dd 75%,#0000FF)") {
       setBackground("linear-gradient(0deg,#f0e6e600, #d6d4dd 75%,#00000F)")
       setHeading("black")
+      setToggle(!toggled)
     }
     else{
       setBackground("linear-gradient(0deg,#f0e6e600, #d6d4dd 75%,#0000FF)")
       setHeading("blue")
+      setToggle(!toggled)
     }
   }
+
 
   return (
     <>
       <Navbar expand="lg" bg="dark" data-bs-theme="dark" sticky="top">
         <Container >
           <Navbar.Brand as={Link} to="/home">React-Marketplace-UI {counter}</Navbar.Brand>
-          
-          <Button onClick={handleClick}>Toggle background</Button>
+          <Button variant="outline-secondary" size="sm" onClick={handleClick}>{toggled ? 'Light Theme' : 'Dark Theme'}</Button>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
@@ -56,3 +61,4 @@ export default NavbarComp;
 //{`${props.num.valueOf()}`}
 // "linear-gradient(0deg,#f0e6e600, #d6d4dd 75%,#FFFFFF)"
 // {() => {setBackground("linear-gradient(0deg,#f0e6e600, #d6d4dd 75%,#FFFFFF)")}}
+//
